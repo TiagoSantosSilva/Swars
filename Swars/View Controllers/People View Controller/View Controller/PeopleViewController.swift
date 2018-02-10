6c968adbc8b15017eacab06277499b60a72155df
Tiago Santos <tiagossilva_95@hotmail.com>
2018-02-10 23:01:47 +0000

    People View Controller & View Model.

--- Swars/View Controllers/People View Controller/View Controller/PeopleViewController.swift
+++ Swars/View Controllers/People View Controller/View Controller/PeopleViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class PeopleViewController: BaseViewController {
-
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.register(PersonCell.self)
+        }
+    }
+    
+    // MARK: - Dispose Bag
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - View Model
+    
+    private var viewModel: PeopleViewModelRepresentable!
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel = PeopleViewModel(dataDependencies: DataDependencies())
+        
+        viewModel.dataSource.catchError { [weak self] error in
+            print(error)
+            return Observable.empty()
+            }
+            .bind(to: collectionView.rx.items(cellIdentifier: PersonCell.identifier, cellType: PersonCell.self)) { (_, model, cell) in
+                cell.configure(with: model)
+            }
+            .disposed(by: disposeBag)
+        
+        // TODO: - Model Selection Action Bind
     }
     
     // MARK: - Setups
---
+++ Swars/View Controllers/People View Controller/View Models/PeopleViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  PeopleViewModel.swift
+//  Swars
+//
+//  Created by Tiago Santos on 10/02/18.
+//  Copyright © 2018 Tiago Santos. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol PeopleViewModelRepresentable {
+    var dataSource: Observable<[PersonCellViewModelRepresentable]> { get }
+}
+
+class PeopleViewModel: PeopleViewModelRepresentable {
+    
+    // MARK: - Dependencies
+    
+    typealias DependenciesList = HasNetworkService
+    private var dataDependencies: DependenciesList?
+    
+    // MARK: - Dispose Bag
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - People Data
+    
+    private var peopleData: [PersonCellViewModelRepresentable]?
+    
+    // MARK: - Input
+    
+    // private var inputDataSource = BehaviorSubject<[PersonCellViewModelRepresentable]>(value: [])
+    
+    // MARK: - Data Source
+    
+    var dataSource: Observable<[PersonCellViewModelRepresentable]>
+    
+    // MARK: - Init
+    
+    init(dataDependencies: DependenciesList) {
+        self.dataDependencies = dataDependencies
+        self.peopleData = [PersonCellViewModelRepresentable]()
+        
+        dataSource = PeopleViewModel.fetchStarWarsPeople(with: "1", dataDependency: dataDependencies).map {
+            $0.results.map {
+                $0.map(PersonCellViewModel.init)
+                }!
+        }
+    }
+    
+    static private func fetchStarWarsPeople(with page: String, dataDependency: DependenciesList) -> Observable<PeoplePage> {
+        
+        let jsonDecoder = JSONDecoder()
+        
+        let peoplePage = fetchStarWarsPeoplePage(with: page, dataDependency: dataDependency).map {
+            try jsonDecoder.decode(PeoplePage.self, from: $0)
+        }
+        
+        return peoplePage
+    }
+    
+    static private func fetchStarWarsPeoplePage(with page: String, dataDependency: DependenciesList) -> Observable<Data> {
+        return dataDependency.networkService.getStarWarsPeople(with: page)
+    }
+}
