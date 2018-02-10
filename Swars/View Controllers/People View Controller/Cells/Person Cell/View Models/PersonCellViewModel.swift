3863501c9a88be870e386dc12f93f194f18f6026
Tiago Santos <tiagossilva_95@hotmail.com>
2018-02-10 23:02:24 +0000

    Person Cell & Person Cell View Model (WIP)

--- Swars/View Controllers/People View Controller/Cells/Person Cell/View Models/PersonCellViewModel.swift
+++ Swars/View Controllers/People View Controller/Cells/Person Cell/View Models/PersonCellViewModel.swift
@@ -10,23 +10,52 @@ import Foundation
 
 
 protocol PersonCellViewModelRepresentable {
-    var personId: String { get }
-    var name: String { get }
-    var species: String { get }
-    var vehicleCount: String { get }
+    var personId: String! { get }
+    var name: String! { get }
+    var species: String! { get }
+    var vehicleCount: String! { get }
 }
 
 struct PersonCellViewModel: PersonCellViewModelRepresentable {
     
-    let personId: String
-    let name: String
-    let species: String
-    let vehicleCount: String
+    internal var personId: String!
+    internal var name: String!
+    internal var species: String!
+    internal var vehicleCount: String!
     
     init(person: Person) {
-        personId = person.personId
-        name = person.name.capitalized
-        species = person.species.capitalized
-        vehicleCount = person.vehicleCount.capitalized
+        self.personId = getPersonId(person: person)
+        self.name = getPersonName(person: person)
+        self.species = getPersonSpecies(person: person)
+        self.vehicleCount = getVehicleCount(person: person)
+    }
+    
+    // TODO: - Substring the id
+    private func getPersonId(person: Person) -> String {
+        guard let personId = person.url else {
+            return ""
+        }
+        return personId
+    }
+    
+    private func getPersonName(person: Person) -> String {
+        guard let personName = person.name else {
+            return ""
+        }
+        return personName
+    }
+    
+    private func getPersonSpecies(person: Person) -> String {
+        guard let personSpecies = person.species?.first else {
+            return ""
+        }
+        return personSpecies
+    }
+    
+    private func getVehicleCount(person: Person) -> String {
+        guard let vehicleCount = person.vehicles?.count else {
+            return ""
+        }
+        return String(describing: vehicleCount)
     }
 }
