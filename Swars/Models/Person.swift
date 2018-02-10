5cc0f939655641579c7b20870392766aeb9423dd
Tiago Santos <tiagossilva_95@hotmail.com>
2018-02-10 23:00:05 +0000

    Person model.

--- Swars/Models/Person.swift
+++ Swars/Models/Person.swift
@@ -8,10 +8,41 @@
 
 import Foundation
 
-struct Person {
+struct Person: Decodable {
     
-    let personId: String
-    let name: String
-    let vehicleCount: String
-    let species: String
+    let name: String?
+    let height: String?
+    let mass: String?
+    let hairColor: String?
+    let skinColor: String?
+    let eyeColor: String?
+    let birthYear: String?
+    let gender: String?
+    let homeworld: String?
+    let films: [String]?
+    let species: [String]?
+    let vehicles: [String]?
+    let starships: [String]?
+    let created: String?
+    let edited: String?
+    let url: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case height = "height"
+        case mass = "mass"
+        case hairColor = "hair_color"
+        case skinColor = "skin_color"
+        case eyeColor = "eye_color"
+        case birthYear = "birth_year"
+        case gender = "gender"
+        case homeworld = "homeworld"
+        case films = "films"
+        case species = "species"
+        case vehicles = "vehicles"
+        case starships = "starships"
+        case created = "created"
+        case edited = "edited"
+        case url = "url"
+    }
 }
